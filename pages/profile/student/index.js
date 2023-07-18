0db8282584993d20896dc9083e268b42fc319e8c
RICHITHA REDDY <104646601+RichithaaReddy@users.noreply.github.com>
2023-07-18 14:45:43 +0530

    frontend

--- pages/profile/student/index.js
+++ pages/profile/student/index.js
@@ -3,18 +3,42 @@ import Navbar from '../../../components/Navbar'
 import { getLoginSession } from '../../../lib/auth';
 import { findUser } from '../../../lib/user';
 import crypto from "crypto";
+import { HiOutlineNewspaper } from 'react-icons/hi';
 
-export default function index({userDetails}) {
+
+export default function Index({userDetails}) {
     const user = JSON.parse(userDetails)
     
-    
+    console.log(user)
+    console.log(user.rollNumber)
     const [position ,setPosition] = useState("Student")
   return (
     <>
     <Navbar/>
-    <div className='flex flex-col mt-24'>
-            {console.log("user",user)}
+    <div className='min-h-screen'>
+    <div className='flex mt-[11.1vh]'>
+      <div className='w-[25%] bg-gray-800 h-screen'>
+          <p className='bg-violet-800 text-white text-center py-3 tracking-widest'>STUDENT PROFILE</p>
+          <div className='flex px-4 py-4'>
+            <div>
+              <img className='h-24 w-24 rounded-full' src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/>
+            </div>
+            <div className='text-white py-8 px-6'>
+              <p className='text-xl tracking-wide'>{user.rollNumber.value}</p>
+            </div>
+          </div>
+          <div className='px-5 py-3'>
+            <div className='flex'>
+              <div><HiOutlineNewspaper/></div>
+              <div></div>
+            </div>
 
+          </div>
+      </div>
+      <div className='w-[70%]'>
+
+      </div>
+    </div>
     </div>
     </>
   )
